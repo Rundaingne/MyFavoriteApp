34fd14b9b8def7f6bd010d9116e7710766530386
Erich Kumpunen <erichtheviolin@gmail.com>
2019-03-20 10:26:28 -0600

    Adds model objects and storyboards

---
+++ MyFavoriteApp/Models/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  MyFavoriteApp
+//
+//  Created by Brooke Kumpunen on 3/20/19.
+//  Copyright © 2019 Rund LLC. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    let name: String
+    let favoriteApp: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case favoriteApp = "favApp"
+    }
+    
+}
