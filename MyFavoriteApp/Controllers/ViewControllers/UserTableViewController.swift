de3df9fa481fe7547ca174a25dcda2945d03183a
Erich Kumpunen <erichtheviolin@gmail.com>
2019-03-20 12:32:02 -0600

    Working product

--- MyFavoriteApp/Controllers/ViewControllers/UserTableViewController.swift
+++ MyFavoriteApp/Controllers/ViewControllers/UserTableViewController.swift
@@ -12,42 +12,62 @@ class UserTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.estimatedRowHeight = 45
+        tableView.rowHeight = UITableView.automaticDimension
+        UserController.shared.getUsers { (_) in
+            DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+        }
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return UserController.shared.users.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userPostCell", for: indexPath)
         let user = UserController.shared.users[indexPath.row]
-
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.favoriteApp
         return cell
     }
     
     func presentAlertController() {
+        let alertController = UIAlertController(title: "Create a post", message: "What's your favorite app?", preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Username"
+        }
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Message"
+        }
         
+        let postAction = UIAlertAction(title: "Post", style: .default) { (_) in
+            guard let username = alertController.textFields?[0].text,
+                let favoriteApp = alertController.textFields?[1].text else {return}
+            UserController.shared.postUser(name: username, favoriteApp: favoriteApp, completion: { (_) in
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+            })
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(postAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
     }
 
 
     //MARK: - Actions
     @IBAction func addPostButtonTapped(_ sender: UIBarButtonItem) {
         //We will be presenting an alerty controller here.
+        presentAlertController()
         
     }
 
